b10a2736bd7146b0c4e2da009583e97e2cff3212
panki27 <panki312@gmail.com>
2017-10-26 15:08:51 +0200

    Umlauts fixed

--- entschuldiGen.py
+++ entschuldiGen.py
@@ -2,7 +2,7 @@
 # coding=iso-8859-1
 import random, datetime
 daysOfWeek=["Montag","Dienstag","Mittwoch","Donnerstag","Freitag","Samstag","Sonntag"]
-excuseList = ["Wegen starken Magenschmerzen konnte ich den Schulunterricht nicht wahrnehmen.",u"Aufgrund von starken Kopfschmerzen war es mir nicht möglich, den Unterricht zu besuchen.","Da ich derzeit an einer Magendarm-Erkrankung leide, bin ich dem Unterricht fern geblieben.","Aufgrund eines grippalen Infektes musste ich die Schule meiden.","Bedingt durch akute Kopf- und Gliederschmerzen konnte ich den Unterricht nicht besuchen.",u"Wegen eines Zahnarzttermines war es mir nicht möglich, die Anwesenheit im Unterricht zu erfüllen."]
+excuseList = ["Wegen starken Magenschmerzen konnte ich den Schulunterricht nicht wahrnehmen.",u"Aufgrund von starken Kopfschmerzen war es mir nicht moeglich, den Unterricht zu besuchen.","Da ich derzeit an einer Magendarm-Erkrankung leide, bin ich dem Unterricht fern geblieben.","Aufgrund eines grippalen Infektes musste ich die Schule meiden.","Bedingt durch akute Kopf- und Gliederschmerzen konnte ich den Unterricht nicht besuchen.",u"Wegen eines Zahnarzttermines war es mir nicht moeglich, die Anwesenheit im Unterricht zu erfuellen."]
 date = raw_input("An welchem Datum hast du gefehlt? ")
 frame = raw_input("Den (g)anzen Tag, (a)nfangs oder (e)nde? ")
 if(frame == "a" or frame == "e"):
@@ -13,11 +13,11 @@ dow = datetime.datetime(int(dateList[2]),int(dateList[1]),int(dateList[0])).week
 
 print("Sehr geehrter Herr Maass,")
 if (frame == "a"):
-	print("hiermit entschuldige ich meine Verspätung im "+ course + " Unterricht am "+ daysOfWeek[dow]+ ', den ' + date + ".")
+	print("hiermit entschuldige ich meine Verspaetung im "+ course + " Unterricht am "+ daysOfWeek[dow]+ ', den ' + date + ".")
 elif(frame == "e"):
 	print("hiermit entschuldige ich mein Fehlen im " + course + " Unterricht am "+ daysOfWeek[dow]+ ', den ' + date + ".")
 else:
-	#TODO: Wochentag einfügen
+	#TODO: Wochentag einfuegen
 	print("hiermit entschuldige ich mein Fehlen am "+ daysOfWeek[dow]+ ', den ' + date + ".")
 rng = random.randint(0, len(excuseList)-1)
 print(excuseList[rng])
